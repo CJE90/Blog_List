781d108877c5d712aae55d5e8f970f13fd8f21d7
CJE90 <mrktup@hotmail.com>
2022-01-02 12:57:31 -0500

    refactoring some tests, blog controller, and blog model

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -45,6 +45,66 @@ test('a specific blog is returned within the returned blogs', async () => {
     expect(titles).toContain('Test Blog')
 })
 
+test('Verify unique identifier property is "id"', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined();
+})
+
+describe('Blog posting function', () => {
+    const blogEntryToPost = {
+        title: "Posting from test suite",
+        author: "Computer Overlords",
+        url: "www.no.com",
+        likes: 2
+    }
+    const blogEntryToPostWithNoLikes = {
+        title: "Posting from test suite",
+        author: "Computer Overlords",
+        url: "www.no.com"
+    }
+
+    const blogWithoutRequiredFields = {
+        author: "no one",
+        likes: 0
+    }
+
+    test('A valid blog post can be added', async () => {
+        await api
+            .post('/api/blogs')
+            .send(blogEntryToPost)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const response = await api.get('/api/blogs')
+
+        const titles = response.body.map(blogs => blogs.title)
+
+        expect(response.body).toHaveLength(initialBlogs.length + 1)
+        expect(titles).toContain('Posting from test suite')
+    })
+
+    test('Likes will be set to 0 when positing blog with no likes', async () => {
+        await api
+            .post('/api/blogs')
+            .send(blogEntryToPostWithNoLikes)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const response = await api.get('/api/blogs')
+        const likes = response.body.map(blog => blog.likes)
+        console.log(likes)
+        expect(likes[likes.length - 1]).toEqual(0)
+    })
+
+    test('Post without required fields will return 400', async () => {
+        await api
+            .post('/api/blogs')
+            .send(blogWithoutRequiredFields)
+            .expect(400)
+
+    })
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
